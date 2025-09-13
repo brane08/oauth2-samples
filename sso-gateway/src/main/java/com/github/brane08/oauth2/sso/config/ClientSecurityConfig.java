2c2306f33af0931697f425bc3fb0cfebf1f3d49a
Bhushan Rane <ekpravasi@gmail.com>
2025-09-13 22:44:05 +0100

    third checkpoint with redis caching

--- sso-gateway/src/main/java/com/github/brane08/oauth2/sso/config/ClientSecurityConfig.java
+++ sso-gateway/src/main/java/com/github/brane08/oauth2/sso/config/ClientSecurityConfig.java
@@ -2,11 +2,15 @@ package com.github.brane08.oauth2.sso.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.savedrequest.WebSessionServerRequestCache;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebFluxSecurity
@@ -15,10 +19,15 @@ public class ClientSecurityConfig {
 	@Bean
 	public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
 		// @formatter:off
+		var requestCache = new WebSessionServerRequestCache();
+		var contextRepo  = new WebSessionServerSecurityContextRepository();
 		var csrfRepo = CookieServerCsrfTokenRepository.withHttpOnlyFalse();
 		csrfRepo.setCookiePath("/");
         http.csrf(csrf -> csrf
-				.csrfTokenRepository(csrfRepo))
+				.csrfTokenRepository(csrfRepo)
+			)
+			.securityContextRepository(contextRepo)
+			.requestCache(rc -> rc.requestCache(requestCache))
 			.authorizeExchange(ae -> ae
 				.pathMatchers("/actuator/**", "/assets/**", "/favicon.ico").permitAll()
 				.pathMatchers("/login", "/logout", "/oauth2/**").permitAll()
@@ -27,5 +36,10 @@ public class ClientSecurityConfig {
 			.oauth2Login(Customizer.withDefaults());
 		// @formatter:on
 		return http.build();
+	}
+
+	@Bean
+	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+		return new GenericJackson2JsonRedisSerializer();
 	}
 }
